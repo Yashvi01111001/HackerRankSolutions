ef421ef4fcf6c035153e29fa38723495006a5120
Yashvi Bhadania <yashvijayesh@gmail.com>
2024-05-15 13:56:56 +0300

    Mean Var and Std file
    Logic for mean var and std

---
+++ Numpy/MeanVarSTD.py
@@ -0,0 +1,22 @@
+import numpy
+matrix = []
+
+#Getting values for N and M from space separated user input
+N, M = map(int, input().split())
+# print(N, M)
+
+for _ in range(N):
+    row = list(map(float, input().split()))
+    # print(row)
+    matrix.append(row)
+# print(matrix) #now we've gotten the input in the desired format to pass it to the numpy functions, i.e. nested list
+
+mean_result = numpy.mean(matrix, axis=1)
+var_result = numpy.var(matrix, axis=0)
+std_result = numpy.std(matrix, axis=None)
+std_result = round(std_result, 11)
+print(mean_result)
+print(var_result)
+print(std_result)
+
+#--------Yashvi Bhadania--------
