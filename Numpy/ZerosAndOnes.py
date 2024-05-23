8e50bd155c8101c34d903d7827e81c0600df643a
Yashvi Bhadania <yashvijayesh@gmail.com>
2024-05-23 08:50:34 +0300

    Create ZerosAndOnes.py
    Pushed file plus logic

---
+++ Numpy/ZerosAndOnes.py
@@ -0,0 +1,14 @@
+import numpy
+
+inp = list(map(int, input().split()))
+
+array_of_zeros = numpy.zeros((inp), dtype=int)
+array_of_ones = numpy.ones((inp), dtype=int)
+
+print(array_of_zeros)
+print(array_of_ones)
+
+# # INPUT:
+# 4 4 4
+
+#--------Yashvi Bhadania--------
