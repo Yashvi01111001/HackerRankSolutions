2eaf126209de8c6b077c08b17b6e1d3dc23c787b
Yashvi Bhadania <yashvijayesh@gmail.com>
2024-05-27 08:13:10 +0300

    Transpose and Flatten
    Code commit

---
+++ Numpy/TransposeAndFlatten.py
@@ -0,0 +1,20 @@
+import numpy
+
+N, M = list(map(int, input().split()))
+
+my_array = [[int(i) for i in input().split()] for x in range(N)]
+# print(my_array)
+
+#transposing array
+print(numpy.transpose(my_array))
+
+#flattening array
+my_array = numpy.array(my_array)
+print(my_array.flatten())
+
+# # INPUT:
+# 2 2
+# 1 2
+# 3 4
+
+#--------Yashvi Bhadania--------
