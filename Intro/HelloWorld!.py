9d854920b6e6101adbc0daf0283e1f27408aee84
Yashvi Bhadania <yashvijayesh@gmail.com>
2024-06-30 13:29:05 +0300

    Hello World!!
    Code commit!!

---
+++ Intro/HelloWorld!.py
@@ -0,0 +1,10 @@
+# Hey There! Thank you for coming here.
+# I hope to give you the invaluable guidance/ aid you need in solving challenges.
+# We all begin from Variables! or somewhere :)
+
+# Creating a variable to store the string to print:
+my_stringg = "Hello, World!"
+print(my_stringg)
+
+# Wishing you success on your Python journey!
+#--------Yashvi Bhadania--------
