4b1a81c4b4af201d39c8e8210c53d424c6b61f3c
Yashvi Bhadania <yashvijayesh@gmail.com>
2024-07-01 10:07:21 +0300

    Arithmetic Operators
    Code commit

---
+++ Intro/ArithmeticOperators.py
@@ -0,0 +1,15 @@
+# Use Pypy3
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+print(a+b)
+print(a-b)
+print(a*b)
+
+
+# # SAMPLE INPUT:
+# 3
+# 2
+
+#--------Yashvi Bhadania--------
