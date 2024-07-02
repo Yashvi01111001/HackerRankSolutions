d6a9455fa26971b046189894b7ce03f09c1fc83a
Yashvi Bhadania <yashvijayesh@gmail.com>
2024-07-02 08:54:20 +0300

    Loops
    Code commit

---
+++ Intro/Loops.py
@@ -0,0 +1,16 @@
+# SRC: https://www.hackerrank.com/challenges/python-loops/problem?isFullScreen=true
+
+# Use Pypy 3
+
+if __name__ == '__main__':
+    n = int(input())
+    
+# Looping to print the square of each number
+for i in range(n):
+    print(i**2)
+
+
+# # SAMPLE INPUT:
+# 5
+
+#--------Yashvi Bhadania--------
