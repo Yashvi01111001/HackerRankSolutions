cf02644803c9c9aaba69c75f17fde293e3223374
Yashvi Bhadania <yashvijayesh@gmail.com>
2024-07-04 20:04:39 +0300

    Python Division
    Code commit

---
+++ Intro/PythonDivision.py
@@ -0,0 +1,13 @@
+# SRC: https://www.hackerrank.com/challenges/python-division/problem?isFullScreen=true
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+print(int(a/b))
+print(float(a/b))
+
+# # SAMPLE INPUT:
+# 4
+# 3
+
+#--------Yashvi Bhadania--------
