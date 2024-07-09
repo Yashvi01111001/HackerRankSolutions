2ca3a7617d2b2a0c06c75646c90af08c2a19e2ca
Yashvi Bhadania <yashvijayesh@gmail.com>
2024-07-09 19:51:59 +0300

    Set.add()
    Code commit

---
+++ Sets/Set.add().py
@@ -0,0 +1,29 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+# N = int(input())
+# stamps = list(input() for i in range(N))
+# # print(stamps)
+
+# s = set(stamps)
+# # print(s)
+
+# count = len(s)
+# print(count)
+
+s = set()
+for i in range(int(input())):
+    s.add(input())
+    
+print(len(s))
+
+# # INPUT:
+# 7
+# UK
+# China
+# USA
+# France
+# New Zealand
+# UK
+# France 
+
+#--------Yashvi Bhadania--------
