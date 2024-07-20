2448450a3b045101d3c4095c84f10f23b77ec5c3
Yashvi Bhadania <yashvijayesh@gmail.com>
2024-07-20 17:07:04 +0300

    Create IntegersComeInAllSizes.py
    Code Commit

---
+++ Math/IntegersComeInAllSizes.py
@@ -0,0 +1,16 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+
+print(pow(a, b) + pow(c, d))
+
+# # SAMPLE INPUT:
+# 9
+# 29
+# 7
+# 27
+
+#--------Yashvi Bhadania--------
