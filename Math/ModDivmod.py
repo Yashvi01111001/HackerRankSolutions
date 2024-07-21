b8278fe372bb02939d3eb8584fcb6732751d169b
Yashvi Bhadania <yashvijayesh@gmail.com>
2024-07-21 22:53:00 +0300

    Create ModDivmod.py
    Code commit

---
+++ Math/ModDivmod.py
@@ -0,0 +1,17 @@
+# SRC: https://www.hackerrank.com/challenges/python-mod-divmod/problem?isFullScreen=true
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+a = int(input())
+b = int(input())
+
+print(a // b)
+print(a % b)
+print(divmod(a, b))
+
+
+# # SAMPLE INPUT:
+# 177
+# 10
+
+#--------Yashvi Bhadania--------
