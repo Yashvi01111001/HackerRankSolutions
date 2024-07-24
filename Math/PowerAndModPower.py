6a0ce4d76516413e306d44e6c9c441e66636ce4e
Yashvi Bhadania <yashvijayesh@gmail.com>
2024-07-24 19:59:04 +0300

    Create PowerAndModPower.py
    Code Commit

---
+++ Math/PowerAndModPower.py
@@ -0,0 +1,17 @@
+# SRC: https://www.hackerrank.com/challenges/python-power-mod-power/problem?isFullScreen=true
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+a = int(input())
+b = int(input())
+m = int(input())
+
+print(pow(a, b))
+print(pow(a, b, m))
+
+# # SAMPLE INPUT:
+# 3
+# 4
+# 5
+
+#--------Yashvi Bhadania--------
