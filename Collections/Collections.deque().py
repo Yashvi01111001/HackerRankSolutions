63da258948209be746f46c48ebcec4265c7b0f2a
Yashvi Bhadania <75441982+Yashvi01111001@users.noreply.github.com>
2025-01-08 20:21:22 +0530

    Update Collections.deque().py

--- Collections/Collections.deque().py
+++ Collections/Collections.deque().py
@@ -1,3 +1,27 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+from collections import deque
+
+N = int(input())
+my_deque = deque()
+
+for _ in range(N):
+    # methods, values = input().split
+    # Split the line into a list of parts:
+    line = input().split()
+    # The first part is always the method:
+    method = line[0]
+    
+    if method == "append":
+        my_deque.append(line[1])
+    if method == "appendleft":
+        my_deque.appendleft(line[1])
+    if method == "pop":
+        my_deque.pop()
+    if method == "popleft":
+        my_deque.popleft()
+        
+print(*my_deque)  
+# The asterisk (*), when used before an iterable (like a list, tuple, or deque) in a function call, performs unpacking. It takes the elements of the iterable and passes them to the function as separate arguments.
 
 # # SAMPLE INPUT:
 # 6
