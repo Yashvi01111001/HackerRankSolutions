50a23f0f3fbf57d6a33f14a442d6e566cc9d65ca
Yashvi Bhadania <75441982+Yashvi01111001@users.noreply.github.com>
2025-01-12 12:13:17 +0530

    Update PilingUp.py

--- Collections/PilingUp.py
+++ Collections/PilingUp.py
@@ -1,4 +1,31 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+from collections import deque
 
+T = int(input())
+
+for _ in range(T):
+    n = int(input())
+    side_lengths = deque(map(int, input().split()))
+    # print(*side_lengths)
+    pile = []
+    
+    while side_lengths:
+        
+        # Check which end of the deque has the larger side length (left or rightmost):
+        if side_lengths[0] <= side_lengths[-1]:
+            # Removing rightmost cube and adding to pile list
+            pile.append(side_lengths.pop())
+            
+        else:
+            # Removing leftmost cube and adding to pile list
+            pile.append(side_lengths.popleft())
+    
+    # Creating a new list that is a sorted (descending) copy of pile:   
+    if pile == sorted(pile, reverse=True):
+        print("Yes")
+    else:
+        print("No")
+            
 # # SAMPLE INPUT:
 # STDIN        Function
 # -----        --------
