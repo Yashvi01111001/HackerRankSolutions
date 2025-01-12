d824420620d588485f11a7a0642903f4cedcde56
YB <yashvibhadania@Yashvis-MacBook-Air.local>
2025-01-12 12:12:14 +0530

    pilling up

---
+++ Collections/PilingUp.py
@@ -0,0 +1,10 @@
+
+# # SAMPLE INPUT:
+# STDIN        Function
+# -----        --------
+# 2            T = 2
+# 6            blocks[] size n = 6
+# 4 3 2 1 3 4  blocks = [4, 3, 2, 1, 3, 4]
+# 3            blocks[] size n = 3
+# 1 3 2        blocks = [1, 3, 2]
+#--------Yashvi Bhadania--------
