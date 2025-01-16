f9a5a15f7f679c1fac7ed982337267faf8d2d993
Yashvi Bhadania <75441982+Yashvi01111001@users.noreply.github.com>
2025-01-16 21:46:27 +0530

    Update Exceptions.py

--- Errors and Exceptions/Exceptions.py
+++ Errors and Exceptions/Exceptions.py
@@ -1,3 +1,17 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+T = int(input())
+for _ in range(T):
+    a, b = input().split()
+    # print(a,b)
+    try:
+        a = int(a)
+        b = int(b)
+        print(a//b)
+    except ZeroDivisionError as e:
+        print("Error Code:",e)
+    except ValueError as e:
+        print("Error Code:",e)
+
 # # SAMPLE INPUT:
 # 08 05 2015
 
