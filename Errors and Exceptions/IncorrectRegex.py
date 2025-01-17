5e23468ce98f777044b8e1ca2c0bbc340e34e8ac
Yashvi Bhadania <75441982+Yashvi01111001@users.noreply.github.com>
2025-01-17 22:06:56 +0530

    Update IncorrectRegex.py

--- Errors and Exceptions/IncorrectRegex.py
+++ Errors and Exceptions/IncorrectRegex.py
@@ -1,3 +1,13 @@
+# # Enter your code here. Read input from STDIN. Print output to STDOUT
+import re
+
+n = int(input())
+for i in range(n):
+    try:
+        a = (re.compile(input()))
+        print(bool(a))
+    except re.error:
+        print('False') 
 
 # # SAMPLE INPUT:
 # 08 05 2015
