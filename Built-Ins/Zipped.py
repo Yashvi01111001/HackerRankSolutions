614eb083aa10064d02e40988f18e20dfb0efdf29
Yashvi Bhadania <75441982+Yashvi01111001@users.noreply.github.com>
2025-01-20 22:32:43 +0530

    Update Zipped.py

--- Built-Ins/Zipped.py
+++ Built-Ins/Zipped.py
@@ -1,3 +1,25 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+# N (students), X (subjects)
+N, X = map(int, input().split())
+grade_all_subjects=[]
+for _ in range (X):
+    grades = list(map(float, input().split()))
+    # print(grades)
+    grade_all_subjects.append(grades)
+    
+# Transpose list of lists to get grades by student
+grades_by_student = list(zip(*grade_all_subjects))
+# print(grades_by_student)
+
+for _ in grades_by_student:
+    sum=0
+    cnt=0
+    # print(list(_))
+    for x in _:
+        sum+=x
+        cnt+=1
+    print(sum/cnt)
 
 # # SAMPLE INPUT:
 # 5 3
