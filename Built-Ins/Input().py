f14ee329d0e0c0cd7c31dfa029428af84cb1d4f4
Yashvi Bhadania <75441982+Yashvi01111001@users.noreply.github.com>
2025-01-21 23:04:19 +0530

    Update Input().py

--- Built-Ins/Input().py
+++ Built-Ins/Input().py
@@ -1,3 +1,10 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+x, k = map(int, input().split())
+P = input()
+# print(P)
+P = P.replace('x', str(x))
+P_result = eval(P)
+print(P_result==k)
 
 # # SAMPLE INPUT:
 # 1 4
