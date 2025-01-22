bef741f7b4f2dfcccc5548f0fec2ef6c575e3a31
Yashvi Bhadania <75441982+Yashvi01111001@users.noreply.github.com>
2025-01-23 00:04:30 +0530

    Update AthleteSort.py

--- Built-Ins/AthleteSort.py
+++ Built-Ins/AthleteSort.py
@@ -1,3 +1,48 @@
+#!/bin/python3
+ 
+import math
+import os
+import random
+import re
+import sys
+
+if __name__ == '__main__':
+    nm = input().split()
+
+    n = int(nm[0])
+
+    m = int(nm[1])
+
+    arr = []
+
+    for _ in range(n):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    k = int(input())
+    # print(arr)
+    # k_column_values = []
+    # for _ in arr:
+    #     k_column_values.append(_[k])
+    # k_column_values = sorted(k_column_values)
+    
+    # for x in k_column_values:
+    #     for _ in arr:
+    #         if _[k]==x:
+    #             print(*_)
+    # THE ABOVE CODE DID NOT PASS TEST CASE 2 WHICH REQUIRED MAINTAINING INPUT ORDER
+    
+    # Sort the array using a lambda function as the key
+    
+    # arr.sort(): This is the built-in sort() method for lists in Python. It sorts the list in place, meaning it modifies the original list directly.
+    
+    # LAMBDA FORMAT: lambda arguments: expression
+    # lambda row: row[k] means:
+    # lambda row: Define a lambda function that takes one argument named row.
+    # row[k]: The expression that the function evaluates and returns. It accesses the element at index k of the row list.
+    arr.sort(key=lambda row: row[k])
+
+    for row in arr:
+        print(*row)
 
 # # SAMPLE INPUT:
 # 5 3
