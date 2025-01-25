abefeddb5e435aefde3ad0da7855abb5e27b38e5
Yashvi Bhadania <75441982+Yashvi01111001@users.noreply.github.com>
2025-01-25 14:48:49 +0530

    Update AnyOrAll.py

--- Built-Ins/AnyOrAll.py
+++ Built-Ins/AnyOrAll.py
@@ -1,3 +1,7 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+n = input()
+integers_list = list(map(int, input().split()))
+print(all(i>0 for i in integers_list) and any(str(i) == str(i)[::-1] for i in integers_list))
 
 # # SAMPLE INPUT:
 # 5
