aa2e470cdad16fdc1b9aadddc717a814711522d9
Yashvi Bhadania <75441982+Yashvi01111001@users.noreply.github.com>
2025-01-26 22:41:50 +0530

    Update ginortS.py

--- Built-Ins/ginortS.py
+++ Built-Ins/ginortS.py
@@ -1,11 +1,14 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+s = input()
+
+lowercase = ''.join(sorted(c for c in s if c.islower()))
+uppercase = ''.join(sorted(c for c in s if c.isupper()))
+odd_digits = ''.join(sorted(str(d) for d in [int(c) for c in s if c.isdigit() and int(c) % 2 != 0]))
+even_digits = ''.join(sorted(str(d) for d in [int(c) for c in s if c.isdigit() and int(c) % 2 == 0]))
+
+print(lowercase+uppercase+odd_digits+even_digits)
 
 # # SAMPLE INPUT:
-# 5 3
-# 10 2 5
-# 7 1 0
-# 9 9 9
-# 1 23 12
-# 6 5 9
-# 1
+# Sorting1234
 
 #--------Yashvi Bhadania--------
