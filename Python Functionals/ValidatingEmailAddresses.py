89239eaee3120eead0f42e54abdf6f8143d2b131
Yashvi Bhadania <75441982+Yashvi01111001@users.noreply.github.com>
2025-01-28 12:36:45 +0530

    Update ValidatingEmailAddresses.py

--- Python Functionals/ValidatingEmailAddresses.py
+++ Python Functionals/ValidatingEmailAddresses.py
@@ -1,3 +1,25 @@
+import re
+def fun(s):
+    # return True if s is a valid email, else return False
+    pattern = r'^[a-zA-Z0-9_-]+@[a-zA-Z0-9]+\.[a-zA-Z]{1,3}$'
+    
+    if (re.match(pattern, s)):
+        return True
+    else:
+        return False
+
+def filter_mail(emails):
+    return list(filter(fun, emails))
+
+if __name__ == '__main__':
+    n = int(input())
+    emails = []
+    for _ in range(n):
+        emails.append(input())
+
+filtered_emails = filter_mail(emails)
+filtered_emails.sort()
+print(filtered_emails)
 
 # # SAMPLE INPUT:
 # 3
