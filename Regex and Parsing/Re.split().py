da81372f6ec2c9b550e19058b098c8aa5a741f1a
Yashvi01111001 <yashvijayesh@gmail.com>
2025-07-10 20:31:09 +0530

    re.split() solution

---
+++ Regex and Parsing/Re.split().py
@@ -0,0 +1,9 @@
+regex_pattern = r"[,.]"	# Do not delete 'r'.
+
+import re
+print("\n".join(re.split(regex_pattern, input())))
+
+# # SAMPLE INPUT:
+# 100,000,000.000
+
+#--------Yashvi Bhadania--------
